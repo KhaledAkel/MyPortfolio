6bf8d6124b53021f7655903d17f72c79478b04d8
khaledakel <khaled-akel@outlook.com>
2024-09-20 00:35:06 +0200

    Update

--- src/components/Contactme.jsx
+++ src/components/Contactme.jsx
@@ -4,11 +4,11 @@ import { FaLinkedinIn, FaGithub, FaYoutube   } from "react-icons/fa6";
 
 function Contactme() {
   return (
-    <div className='w-full h-[35%] max-lg:h-fit flex justify-between px-5 underline max-lg:flex-col-reverse'>
-        <div className='flex items-end  gap-x-2 mb-5 text-[30px] font-bold max-lg:justify-center'>
-            <p className='hover:text-red-700 transition-all duration-300 cursor-pointer'>LinkedIn</p>
-            <p className='hover:text-red-700 transition-all duration-300 cursor-pointer'>Github</p>
-            <p className='hover:text-red-700 transition-all duration-300 cursor-pointer'>Youtube</p>
+    <div className='w-full h-[35%] max-lg:h-fit flex justify-between px-5  max-lg:flex-col-reverse'>
+        <div className='flex items-end  gap-x-2 mb-5 text-[30px] font-bold max-lg:justify-center underline'>
+            <a className='hover:text-red-700 transition-all duration-300 cursor-pointer' href="https://www.linkedin.com/in/khaled-akel-98638a250/" target="_blank">LinkedIn</a>
+            <a className='hover:text-red-700 transition-all duration-300 cursor-pointer' href="https://github.com/KhaledAkel" target="_blank">Github</a>
+            <a className='hover:text-red-700 transition-all duration-300 cursor-pointer' href="https://www.youtube.com/@khaledakel7354" target="_blank">Youtube</a>
         </div>
         <div className='text-[150px] font-bold flex flex-col justify-end text-end max-sm:text-[80px]'>
             <div className='lg:-mt-10'>Contact</div>
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -5,9 +5,9 @@ function SocialLinks() {
   return (
     <div className='flex flex-col items-center absolute top-10 right-0 gap-y-4 max-sm:hidden '>
       <div className='bg__dark-blue w-[1px] h-28 rounded-full mb-2' />
-      <a href=""><FaLinkedinIn className='text-[28px] text__dark-blue' /></a>
-      <a href=""><FaGithub className='text-[28px] text__dark-blue ' /></a>
-      <a href=""><FaYoutube className='text-[28px] text__dark-blue' /></a>
+      <a href="https://www.linkedin.com/in/khaled-akel-98638a250/" target="_blank"><FaLinkedinIn className='text-[28px] text__dark-blue' /></a>
+      <a href="https://github.com/KhaledAkel" target="_blank"><FaGithub className='text-[28px] text__dark-blue ' /></a>
+      <a href="https://www.youtube.com/@khaledakel7354" target="_blank"><FaYoutube className='text-[28px] text__dark-blue' /></a>
       <p className='text__dark-blue -rotate-90 mt-5'>Follow me</p>
     </div>
   )
